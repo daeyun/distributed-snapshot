08df145c19e2158616f0c1fa62d8fad693388288
Daeyun Shin <daeyunshin@gmail.com>
2014-02-23 18:53:56 -0600

    MP1 project structure

---
+++ setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='cs425-mp1',
+      version='0.1',
+      description='CS 425 Assignment 1',
+      author='Daeyun Shin, Mike Joon Young Seo',
+      packages=['mp1'])
