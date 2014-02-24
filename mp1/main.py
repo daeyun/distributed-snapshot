27bcc867a0c707dff726f94559d1f596a30b21f6
Daeyun Shin <daeyunshin@gmail.com>
2014-02-24 17:49:26 -0600

    Add trader_process and spawn new processes

--- mp1/main.py
+++ mp1/main.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python3
 import logging
 import sys
+import multiprocessing as mp
+from helpers.networking_helper import pick_free_ports
+from trader_process import *
 
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)
-console_handler = logging.StreamHandler(sys.stdout)
-formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s]"
-                              "[%(levelname)-5.5s]  %(message)s")
-console_handler.setFormatter(formatter)
-logger.addHandler(console_handler)
 
 
 def run():
-    logger.info("Hello")
+    logger.info("Master process started")
+
+    processes = []
+    n_processes = 5
+    ports = pick_free_ports(n_processes)
+
+    for i in range(n_processes):
+        p = mp.Process(target=trader_process, args=(ports, i))
+        p.start()
+        processes.append(p)
+
+    for p in processes:
+        p.join()
+
+
 
 if __name__ == '__main__':
+    logger.setLevel(logging.DEBUG)
+    console_handler = logging.StreamHandler(sys.stdout)
+    formatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s]"
+                                  "[%(levelname)-5.5s]  %(message)s")
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
     try:
         run()
     except:
---
+++ mp1/trader_process.py
@@ -0,0 +1,7 @@
+from mp1.mp1.main import logger
+
+
+def trader_process(ports, id):
+    # logger.info("Process id: ", id)
+    print(id)
+    pass
