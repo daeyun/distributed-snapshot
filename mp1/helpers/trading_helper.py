ddf165915c2242d73ac6e9ce6c707dc7a8693ef8
JOON YOUNG SEO <MikeSeo093@gmail.com>
2014-03-01 17:16:45 -0600

    Working on testing trading_helper finished

--- mp1/helpers/trading_helper.py
+++ mp1/helpers/trading_helper.py
@@ -1,7 +1,15 @@
+import struct
+
 def get_initial_trading_vars(rand):
     widgets = rand.randint(100, 400)
     money = rand.randint(1200, 2500)
     return (widgets, money)
 
 def unpack_list_data(struct_list):
-    return []
+    int_list_len = int(len(struct_list)/4)
+    int_list = []
+    for i in range(int_list_len):
+        int_list.append(struct.unpack('!i', struct_list[i * 4 : i * 4 + 4])[0])
+
+    return int_list
+
--- tests/test_trading_helpers.py
+++ tests/test_trading_helpers.py
@@ -12,14 +12,15 @@ class TestTradingHelpers(TestCase):
 
         packed_int_list = b''
         num = 7
+        int_list = [0, 5, 1, 2, 3, 4, 5]
         for i in range(num):
-            packed_int_list = packed_int_list + struct.pack('!i', i)
+            packed_int_list = packed_int_list + struct.pack('!i', int_list[i])
 
         unpacked_list = unpack_list_data(packed_int_list)
 
         self.assertEqual(len(unpacked_list), num)
 
         for i in range(num):
-            self.assertEqual(unpacked_list[i], i)
+            self.assertEqual(unpacked_list[i], int_list[i])
 
 
