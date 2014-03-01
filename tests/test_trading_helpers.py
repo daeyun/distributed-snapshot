ece30998ff33bcb2eac6b296b62711a0e4b37354
JOON YOUNG SEO <MikeSeo093@gmail.com>
2014-03-01 16:42:31 -0600

    Working on testing trading_helper

---
+++ tests/test_trading_helpers.py
@@ -0,0 +1,10 @@
+import os
+import socket
+from unittest import TestCase
+import sys
+from mp1.mp1.helpers.networking_helper import pick_free_ports
+
+class TestTradingHelpers(TestCase):
+    def test_unpack_list_data(self):
+        struct.pack()
+
