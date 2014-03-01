054ef80af2622a843256504355e0a066044b4cb1
JOON YOUNG SEO <MikeSeo093@gmail.com>
2014-03-01 15:51:09 -0600

    random message passing

--- mp1/trader_process.py
+++ mp1/trader_process.py
@@ -3,15 +3,16 @@ import random
 from mp1.mp1.main import logger
 
 def trader_process(port_mapping, n_processes, id, asset):
-    rand = random.Random
+    rand = random.Random()
     rand.seed(id)
+    buying_probability = rand.uniform(0.01, 0.1)
 
     sockets = []
     backlog = 10
 
-    def send_message(dest_pid):
+    def send_message(dest_pid, message):
         sock = sockets[dest_pid]
-        sock.send(b'hi')
+        sock.send(str(message).encode('utf'))
 
     for i in range(id):
         server_sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -30,13 +31,11 @@ def trader_process(port_mapping, n_processes, id, asset):
         client_sock.connect((addr, destination_port))
         sockets.append(client_sock)
 
-    for i in range(id + 1, n_processes):
-        send_message(i)
-
     for i in range(id):
         sockets[i].settimeout(0.01)
 
     while True:
+        # receiving message
         for i in range(id):
             try:
                 data = sockets[i].recv(1024)
@@ -45,4 +44,18 @@ def trader_process(port_mapping, n_processes, id, asset):
                 pass
             except BlockingIOError:
                 pass
+
+        # sending message
+        buying_attempt = rand.uniform(0, 1)
+        if buying_attempt <= buying_probability:
+            seller = rand.randint(0, n_processes - 1)
+            if seller >= id:
+                seller = seller + 1
+
+            current_money = asset[1]
+            if current_money == 0:
+                pass
+            else:
+                buying_amount = rand.randint(1, current_money)
+                send_message(seller, buying_amount)
     pass
