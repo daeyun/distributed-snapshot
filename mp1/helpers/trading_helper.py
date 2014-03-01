2e1029b01f53abe35f4e0960cb3df1ef65490807
JOON YOUNG SEO <MikeSeo093@gmail.com>
2014-03-01 16:45:24 -0600

    Working on testing trading_helper 2

--- mp1/helpers/trading_helper.py
+++ mp1/helpers/trading_helper.py
@@ -2,3 +2,6 @@ def get_initial_trading_vars(rand):
     widgets = rand.randint(100, 400)
     money = rand.randint(1200, 2500)
     return (widgets, money)
+
+def unpack_list_data(struct_list):
+    pass
